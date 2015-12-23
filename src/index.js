8e90ec72a028636e48c5a0c30f91fae7b31da673
ivraeus <Lukas@ivraeus.com>
2015-12-23 13:46:47 +0100

    rewrite paging class

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 (() => {
 
-  // TODO!
-  // extract gestures in to own helper class.
-  //
-  // class Gesture {
-  //   constructor() {
-  //   }
-  // }
-
   'use strict';
   console.log('init');
 
@@ -16,131 +8,197 @@
   HTMLCollection.prototype.forEach = Array.prototype.forEach;
   HTMLCollection.prototype.map = Array.prototype.map;
 
-  class Page {
-    constructor(element, set) {
-      this._element = element;
-      this._set = set;
+  function getSiblingIfClass(element, className, direction) {
+    let elm = null;
+    if (direction === 'backwards') {
+      elm = element.previousSibling;
+    } else if (direction === 'forwards'){
+      elm = element.nextSibling;
     }
+    if (elm.tagName !== 'DIV') { return false; }
+    if (elm.classList.contains(className)) { return elm; }
+    return false;
   }
 
-  class PageSet {
+  class Page {
     constructor(element) {
-      this._element = element;
-      this._pages   = element.children.map(e => new Page(e, this));
-      this._offset  = 1;
-      this._axis    = (element.classList.contains(
-        'page-set--horizontal')) ? 'x' : 'y';
-      this._element.addEventListener('touchstart', e => this._onTouchStart(e));
+      this._self = element;
+      this._prev = getSiblingIfClass(element, 'page', 'backwards');
+      this._next = getSiblingIfClass(element, 'page', 'forwards');
+      this._rect = element.getBoundingClientRect();
 
-      this._setPosition();
-    }
-
-    _setPosition() {
-      requestAnimationFrame(() => {
-        if (this._axis === 'x') {
-          this._element.scrollLeft = this._offset * window.innerWidth;
-        } else {
-          this._element.scrollTop = this._offset * window.innerHeight;
-        }
-      });
+      element.addEventListener('touchstart', e => this._onTouchStart(e));
     }
 
     _onTouchStart(event) {
 
-      // Store original event for later.
-      let oldEvent = event;
-
-      // prevent default event
       event.preventDefault();
+      event.stopPropagation();
+
+      if (event.touches.length > 1) return true;
 
       let startTime = window.performance.now();
 
-      // true if paint is pending
-      let pendingPaint = false;
+      let pending = false;
 
-      // direction of current swipe
-      let swipeAxis = false;
-
-      let windowSize = {
-        x: window.innerWidth,
-        y: window.innerHeight
-      };
+      const SELF = this._self;
+      const PREV = this._prev;
+      const NEXT = this._next;
+      const RECT = this._rect;
 
       let startPos = {
         x: event.touches[0].pageX,
         y: event.touches[0].pageY
       };
 
-      let deltaPos = {
-        x: 0,
-        y: 0
-      };
+      let deltaPos = { x: 0, y: 0 };
+
+      if (PREV) {
+        PREV.style.cssText = null;
+        PREV.style.left = '-30%';
+        PREV.classList.add('inView');
+      }
+
+      if (NEXT) {
+        NEXT.style.cssText = null;
+        NEXT.style.left = '100%';
+        NEXT.classList.add('inView');
+      }
+
+      SELF.style.cssText = null;
+
+
 
       let onTouchMove = event => {
 
-        deltaPos = {
-          x: event.touches[0].pageX - startPos.x,
-          y: event.touches[0].pageY - startPos.y
-        };
+        event.preventDefault();
+        event.stopPropagation();
 
-        if (!swipeAxis) {
-          if (Math.abs(deltaPos.x) > 20) {
-            swipeAxis = 'x';
-          } else if (Math.abs(deltaPos.y) > 20) {
-            swipeAxis = 'y';
-          }
-        } else if (swipeAxis === this._axis) {
-          // stop event for other targets if this is an allowed swipe
-          event.stopPropagation();
+        if (!pending) {
 
-          // only run if no paint in pipeline.
-          if (!pendingPaint) {
+          deltaPos.x = event.touches[0].pageX - startPos.x;
 
-            // paint swipe here
-            requestAnimationFrame(() => {
+          requestAnimationFrame(() => {
 
-              // scroll correct axis
-              if (this._axis === 'x') {
-                this._element.scrollLeft = -deltaPos.x +
-                  (this._offset * window.innerWidth);
-              } else {
-                this._element.scrollTop = -deltaPos.y +
-                  (this._offset * window.innerHeight);
-              }
-              pendingPaint = false;
-            }); // requestAnimationFrame
-            pendingPaint = true;
-          }
-        } else {
-          return false;
+            if (!PREV && deltaPos.x > 0) deltaPos.x = 0;
+            if (!NEXT && deltaPos.x < 0) deltaPos.x = 0;
+
+            let prevPos = (deltaPos.x > 0) ? deltaPos.x / 3 : 0;
+            let nextPos = (deltaPos.x < 0) ? deltaPos.x : 0;
+            let selfPos = (deltaPos.x < 0) ? deltaPos.x / 3 : deltaPos.x;
+
+            if (PREV) PREV.style.transform =
+              'translateX(' + prevPos + 'px)';
+
+            if (NEXT) NEXT.style.transform =
+              'translateX(' + nextPos + 'px)';
+
+            SELF.style.transform =
+              'translateX(' + selfPos + 'px)';
+
+            pending = false;
+          });
+
+          pending = true;
         }
       };
 
-      let onTouchEnd = event => {
+
+
+      let onTouchEnd = () => {
 
         let deltaTime = window.performance.now() - startTime;
-        let percentage = deltaPos[this._axis] / windowSize[this._axis];
-        let velocity = (percentage * 100) / deltaTime;
+        let percent = (deltaPos.x / RECT.width) * 100;
+        let velocity = percent / deltaTime;
 
-        // swipe if more then half screen movement.
-        // swipe if more then .2 in velocity.
-        if (percentage > 0.5 || velocity > 0.2) {
-          this._offset = Math.max(this._offset - 1, 0);
-        } else if (percentage < -0.5 || velocity < -0.2) {
-          this._offset = Math.min(this._offset + 1, this._pages.length - 1);
+        let velocityThreshold = 0.16;
+        let percentTreshold = 50;
+
+        if (percent !== 0) {
+
+          requestAnimationFrame(() => {
+
+            if (percent > percentTreshold || velocity > velocityThreshold) {
+
+              PREV.style.transition = 'transform 200ms ease-out';
+              PREV.style.transform = 'translateX(30%)';
+              SELF.style.pointerEvents = 'none';
+              SELF.style.transition = 'transform 200ms ease-out';
+              SELF.style.transform = 'translateX(100%)';
+
+            } else if ( percent < -percentTreshold || velocity < -velocityThreshold) {
+
+              NEXT.style.transition = 'transform 200ms ease-out';
+              NEXT.style.transform = 'translateX(-100%)';
+              SELF.style.pointerEvents = 'none';
+              SELF.style.transition = 'transform 200ms ease-out';
+              SELF.style.transform = 'translateX(-30%)';
+
+            } else {
+
+              if (PREV) {
+                PREV.style.pointerEvents = 'none';
+                PREV.style.transition = 'transform 200ms ease-out';
+                PREV.style.transform = 'translateX(0)';
+              }
+
+              if (NEXT) {
+                NEXT.style.pointerEvents = 'none';
+                NEXT.style.transition = 'transform 200ms ease-out';
+                NEXT.style.transform = 'translateX(0)';
+              }
+
+              SELF.style.transition = 'transform 200ms ease-out';
+              SELF.style.transform = 'translateX(0)';
+            }
+
+          });
+
+
+
+          let onTransitionEnd = () => {
+
+            if (percent > percentTreshold || velocity > velocityThreshold) {
+
+              if (NEXT) NEXT.classList.remove('inView');
+              SELF.classList.remove('inView');
+
+            } else if ( percent < -percentTreshold || velocity < -velocityThreshold) {
+
+              if (PREV) PREV.classList.remove('inView');
+              SELF.classList.remove('inView');
+
+            } else {
+
+              if (PREV) PREV.classList.remove('inView');
+              if (NEXT) NEXT.classList.remove('inView');
+
+            }
+
+            SELF.removeEventListener('transitionend', onTransitionEnd);
+          };
+
+          SELF.addEventListener('transitionend', onTransitionEnd);
+
+        } else {
+          if (PREV) PREV.classList.remove('inView');
+          if (NEXT) NEXT.classList.remove('inView');
         }
-
-        this._setPosition();
 
         document.removeEventListener('touchmove', onTouchMove);
         document.removeEventListener('touchend', onTouchEnd);
+        document.removeEventListener('touchcancel', onTouchEnd);
       };
 
       document.addEventListener('touchmove', onTouchMove);
       document.addEventListener('touchend', onTouchEnd);
+      document.addEventListener('touchcancel', onTouchEnd);
     }
   }
 
-  document.querySelectorAll('.page-set').forEach(e => { new PageSet(e); });
+  document.querySelectorAll('.page').forEach(e => { new Page(e); });
+
+
+
 
 })();
