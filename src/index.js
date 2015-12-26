2e7b94d445b0994aface50ed1b783d4ef4b957ee
ivraeus <Lukas@ivraeus.com>
2015-12-26 10:02:31 +0100

    fixed swipe motion

--- src/index.js
+++ src/index.js
@@ -1,144 +1,139 @@
 (() => {
 
-  'use strict';
-  console.log('init');
+'use strict';
+console.log('init');
 
-  // add utility functions to nodelist and HTMLCollection
-  NodeList.prototype.forEach = Array.prototype.forEach;
-  HTMLCollection.prototype.forEach = Array.prototype.forEach;
-  HTMLCollection.prototype.map = Array.prototype.map;
+document.addEventListener('touchstart', e => {
+  e.preventDefault();
+});
 
-  function getSiblingIfClass(element, className, direction) {
-    let elm = null;
-    if (direction === 'backwards') {
-      elm = element.previousSibling;
-    } else if (direction === 'forwards'){
-      elm = element.nextSibling;
-    }
-    if (elm.tagName !== 'DIV') { return false; }
-    if (elm.classList.contains(className)) { return elm; }
-    return false;
+NodeList.prototype.forEach = Array.prototype.forEach;
+
+function getSiblingIfClass(element, className, direction) {
+  let elm = null;
+  if (direction === 'backwards') {
+    elm = element.previousSibling;
+  } else if (direction === 'forwards'){
+    elm = element.nextSibling;
+  }
+  if (elm.tagName !== 'DIV') { return false; }
+  if (elm.classList.contains(className)) { return elm; }
+  return false;
+}
+
+class Page {
+  constructor(element) {
+    this._self = element;
+    this._prev = getSiblingIfClass(element, 'page', 'backwards');
+    this._next = getSiblingIfClass(element, 'page', 'forwards');
+    this._size = element.getBoundingClientRect();
+    this._sx = 0; // touch start
+    this._dx = 0; // touch delta
+    this._draging = false;
+
+    // bind all functions to this
+    [
+      '_onTransitionEnd',
+      '_dragPaintLoop',
+      '_onTouchStart',
+      '_onTouchMove',
+      '_onTouchEnd'
+    ].forEach(f => {
+      this[f] = this[f].bind(this);
+    });
+
+    element.addEventListener('touchstart', this._onTouchStart);
   }
 
-  class Page {
-    constructor(element) {
-      this._self = element;
-      this._prev = getSiblingIfClass(element, 'page', 'backwards');
-      this._next = getSiblingIfClass(element, 'page', 'forwards');
-      this._rect = element.getBoundingClientRect();
-      this._dx = 0;
-
-      element.addEventListener('touchstart', e => this._onTouchStart(e));
-    }
-
-    _onTouchStart(event) {
-      if (event.touches.length > 1) return;
-      event.preventDefault();
-
-      let startTime = window.performance.now();
-      let sx = event.touches[0].pageX;
-      let draging = true;
-      let direction = null;
-
-      const SELF = this._self;
-      const PREV = this._prev;
-      const NEXT = this._next;
-      const RECT = this._rect;
-
-      if (PREV) {
-        PREV.style.cssText = null;
-        PREV.style.left = '-33.333333%';
-        PREV.classList.add('inView', 'inTransit');
-      }
-
-      if (NEXT) {
-        NEXT.style.cssText = null;
-        NEXT.style.left = '100%';
-        NEXT.classList.add('inView', 'inTransit');
-      }
-
-      SELF.style.cssText = null;
-      SELF.classList.add('inTransit');
-
-      this._dx = 0;
-
-
-
-      let dragPaintLoop = () => {
-        if (draging) {
-          requestAnimationFrame(dragPaintLoop);
-
-          let prevPos = (this._dx > 0) ? this._dx / 3 : 0;
-          let nextPos = (this._dx < 0) ? this._dx : 0;
-          let selfPos = (this._dx < 0) ? this._dx / 3 : this._dx;
-
-          if (PREV) PREV.style.transform = 'translateX(' + prevPos + 'px)';
-          if (NEXT) NEXT.style.transform = 'translateX(' + nextPos + 'px)';
-          SELF.style.transform = 'translateX(' + selfPos + 'px)';
-
-        } else {
-
-          let percent = this._dx / RECT.width;
-          let normalized = Math.round(percent) * 100;
-
-          if (this._dx < 0) {
-            SELF.addEventListener('transitionend', onTransitionEnd);
-            if (NEXT) NEXT.style.transition = 'transform 200ms ease-out';
-            if (NEXT) NEXT.style.transform = 'translateX(' + normalized + '%)';
-            SELF.style.transform = 'translateX(' + normalized / 3 + '%)';
-            SELF.style.transition = 'transform 200ms ease-out';
-          } else if (this._dx > 0) {
-            SELF.addEventListener('transitionend', onTransitionEnd);
-            if (PREV) PREV.style.transition = 'transform 200ms ease-out';
-            if (PREV) PREV.style.transform = 'translateX(' + normalized / 3 + '%)';
-            SELF.style.transform = 'translateX(' + normalized + '%)';
-            SELF.style.transition = 'transform 200ms ease-out';
-          } else {
-            if (PREV) PREV.classList.remove('inView');
-            if (NEXT) NEXT.classList.remove('inView');
-          }
-        }
-      };
-
-
-
-      let onTouchMove = event => {
-        event.preventDefault();
-        this._dx = event.touches[0].pageX - sx;
-        if (!PREV && this._dx > 0) this._dx = 0;
-        if (!NEXT && this._dx < 0) this._dx = 0;
-      };
-
-
-
-      let onTransitionEnd = () => {
-        if (PREV) PREV.classList.remove('inTransit');
-        if (NEXT) NEXT.classList.remove('inTransit')
-        SELF.classList.remove('inTransit');
-        SELF.removeEventListener('transitionend', onTransitionEnd);
-      }
-
-
-
-      let onTouchEnd = () => {
-        draging = false;
-        document.removeEventListener('touchmove', onTouchMove);
-        document.removeEventListener('touchend', onTouchEnd);
-        document.removeEventListener('touchcancel', onTouchEnd);
-      };
-
-
-
-      dragPaintLoop();
-      document.addEventListener('touchmove', onTouchMove);
-      document.addEventListener('touchend', onTouchEnd);
-      document.addEventListener('touchcancel', onTouchEnd);
-    }
+  static init() {
+    document.querySelectorAll('.page').forEach(e => {
+      new Page(e);
+    });
   }
 
-  document.querySelectorAll('.page').forEach(e => { new Page(e); });
+  _onTransitionEnd() {
+    if (this._prev) this._prev.classList.remove('page--smooth');
+    if (this._next) this._next.classList.remove('page--smooth', 'page--shadow');
+    this._self.classList.remove('page--smooth', 'page--shadow');
+    this._self.removeEventListener('transitionend', this._onTransitionEnd);
+    document.body.classList.remove('noEvent');
+  }
 
+  _dragPaintLoop() {
 
+    let percent = 0;
 
+    if (this._draging) {
+
+      requestAnimationFrame(this._dragPaintLoop);
+      percent = (this._dx / this._size.width) * 100;
+
+    } else {
+
+      percent = Math.round(this._dx / this._size.width) * 100;
+
+      if (this._dx === 0 || this._dx === 100) {
+
+        this._self.classList.remove('page--shadow');
+        if (this._next) this._next.classList.remove('page--shadow');
+
+      } else {
+
+        this._self.addEventListener('transitionend', this._onTransitionEnd);
+        this._self.classList.add('page--smooth');
+        if (this._prev) this._prev.classList.add('page--smooth');
+        if (this._next) this._next.classList.add('page--smooth');
+        document.body.classList.add('noEvent');
+      }
+    }
+
+    let prevPos = Math.max(-33.3 + (percent / 3), -33.3);
+    let selfPos = (this._dx < 0) ? percent / 3 : percent;
+    let nextPos = Math.min(100 + percent, 100);
+
+    if (this._prev) this._prev.style.transform =
+      'translateX(' + prevPos + '%)';
+
+    this._self.style.transform =
+      'translateX(' + selfPos + '%)';
+
+    if (this._next) this._next.style.transform =
+      'translateX(' + nextPos + '%)';
+  }
+
+  _onTouchStart(event) {
+
+    event.stopPropagation();
+    event.preventDefault();
+    if (event.touches.length > 1) return;
+
+    this._self.classList.add('page--shadow');
+    if (this._next) this._next.classList.add('page--shadow');
+    this._sx = event.touches[0].pageX;
+    this._dx = 0;
+    this._draging = true;
+    this._dragPaintLoop();
+
+    document.addEventListener('touchmove', this._onTouchMove);
+    document.addEventListener('touchend', this._onTouchEnd);
+    document.addEventListener('touchcancel', this._onTouchEnd);
+  }
+
+  _onTouchMove(event) {
+    this._dx = event.touches[0].pageX - this._sx;
+    if (!this._prev && this._dx > 0) this._dx = 0;
+    if (!this._next && this._dx < 0) this._dx = 0;
+  };
+
+  _onTouchEnd() {
+    this._draging = false;
+    document.removeEventListener('touchmove', this._onTouchMove);
+    document.removeEventListener('touchend', this._onTouchEnd);
+    document.removeEventListener('touchcancel', this._onTouchEnd);
+  }
+
+}
+
+Page.init();
 
 })();
